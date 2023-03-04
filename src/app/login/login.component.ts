86f6f816785098d67fb02b7bf836708c83cdd023
Raman Pakhomau <raman.pakhomau@ihsmarkit.com>
2023-03-04 22:18:54 +0100

    feat: add sign up view

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  
+  @ViewChild("loginId") loginId: ElementRef | undefined;
+  @ViewChild("signupId") signupId: ElementRef | undefined;
+  @ViewChild("btn") btn: ElementRef | undefined;
+  
+  public signup(): void {
+    this.loginId!.nativeElement.style.left = "-400px";
+    this.signupId!.nativeElement.style.left = "50px";
+    this.btn!.nativeElement.style.left = "110px";
+  }
+  public login(): void {
+    this.loginId!.nativeElement.style.left = "50px";
+    this.signupId!.nativeElement.style.left = "450px";
+    this.btn!.nativeElement.style.left = "0";
+  }
 }
